c40b504c3b3daba50a35725169e16b37e97eb1da
locotoki <locotoki@gmail.com>
2025-05-19 10:04:33 +0100

    chore: prune backend/alfred/ml & alfred/remediation (S-1)

--- alfred/remediation/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Alfred remediation module."""
-
-from typing import List
-
-from .graphs import create_remediation_graph
-from .settings import get_settings
-
-__all__: List[str] = ["create_remediation_graph", "get_settings"]
