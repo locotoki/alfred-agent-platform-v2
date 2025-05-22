8d06e3423f385f1914edddedefe74798c13d8b16
locotoki <locotoki@gmail.com>
2025-05-22 20:58:40 +0100

    chore: purge ORPHAN slice A1

---
+++ alfred/core/entrypoint.py
@@ -0,0 +1,13 @@
+"""Core entrypoint module."""
+
+from alfred.protocols import Status
+
+
+def main():
+    """Run main entrypoint using new status."""
+    status = Status.ACTIVE
+    print(f"Status: {status}")
+
+
+if __name__ == "__main__":
+    main()
