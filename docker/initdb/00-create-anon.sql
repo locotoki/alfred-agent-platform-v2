cee2c11e698ae332b1f7525debec3dbccdbc3c27
locotoki <locotoki@gmail.com>
2025-05-30 17:19:33 +0100

    fix: proper anon role creation script

--- docker/initdb/00-create-anon.sql
+++ docker/initdb/00-create-anon.sql
@@ -1,4 +1,14 @@
-CREATE ROLE anon NOLOGIN;
-GRANT USAGE ON SCHEMA public TO anon;
+-- Create anon role for PostgREST
+-- This script runs automatically when PostgreSQL container initializes
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'anon') THEN
+        CREATE ROLE anon NOLOGIN;
+    END IF;
+END $$;
+
+GRANT USAGE  ON SCHEMA public       TO anon;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO anon;
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO anon;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+      GRANT SELECT ON TABLES        TO anon;
