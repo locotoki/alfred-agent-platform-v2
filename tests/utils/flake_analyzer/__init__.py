5a3031acfd7a63c2712d59c7a12dcad9721afaeb
locotoki <locotoki@gmail.com>
2025-06-05 09:57:20 +0100

    chore: resurrect flake-detector workflow (no-op skeleton) (#651)

---
+++ tests/utils/flake_analyzer/__init__.py
@@ -0,0 +1,7 @@
+"""
+Flake detector skeleton (#651).
+Parses pytest JSON output (to-be-implemented) and prints a GH summary.
+"""
+def analyze(json_path: str):
+    # TODO: implement parsing logic
+    return {"flakes": []}
